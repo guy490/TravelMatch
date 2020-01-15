c553218ac73ca84aa6bf0e586a6707f685d3fd8f
guy490 <guy8824@gmail.com>
2020-01-16 00:50:18 +0200

    Category Styling with animation

--- src/component/Category.js
+++ src/component/Category.js
@@ -4,16 +4,18 @@ import { Link } from "react-router-dom";
 
 const Category = ({ linkTo, content }) => {
   return (
-    <div
-      className="background"
-      style={{
-        backgroundImage:
-          "url(https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80)"
-      }}>
-      <Link to={linkTo} className="text">
-        {content}
-      </Link>
-    </div>
+    <Link to={linkTo}>
+      <div className="link">
+        <div
+          className="background"
+          style={{
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1428515613728-6b4607e44363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80)"
+          }}
+        ></div>
+        <div className="text">{content}</div>
+      </div>
+    </Link>
   );
 };
 export default Category;
