22d64ae21cb04aa146fe3a2927b2c5c3199380a9
guy490 <guy8824@gmail.com>
2020-01-17 16:12:54 +0200

    Liniar and refactoring fixes

--- src/component/Category.js
+++ src/component/Category.js
@@ -2,17 +2,21 @@ import "../styles/Category.css";
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Category = ({ linkTo, content, background, color }) => {
+const Category = ({ linkTo, content, backgroundURL, gradientColors }) => {
+  const createCategoryStyle = (imageLink, gradientColors) => {
+    return {
+      backgroundImage: `linear-gradient(${gradientColors}),${imageLink}`,
+      backgroundSize: "100% 100%"
+    };
+  };
+
   return (
     <Link to={linkTo}>
       <div className="link">
         <div
           className="background"
-          style={{
-            //background: `${background}`,
-            background: `linear-gradient(${color}),${background}`,
-            backgroundSize: "100% 100%"
-          }}></div>
+          style={createCategoryStyle(backgroundURL, gradientColors)}
+        ></div>
         <div className="text">{content}</div>
       </div>
     </Link>
