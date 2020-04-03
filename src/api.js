37c81df41f5148f5593f7ded9c46f62621b6eddd
guy490 <guy8824@gmail.com>
2020-04-03 19:09:48 +0200

    First deploying test

--- src/api.js
+++ src/api.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import openSocket from "socket.io-client";
 
+// const server = axios.create({
+//   baseURL: "http://localhost:3001"
+// });
 const server = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: "https://travel-match-project.herokuapp.com/"
 });
 
-const socket = openSocket("ws://localhost:3001");
+// const socket = openSocket("ws://localhost:3001");
+const socket = openSocket("wss://travel-match-project.herokuapp.com/");
 
 export { server, socket };
