274478e4049d39bcb6b4a4e19b1cd94ff3f4c350
guy490 <guy8824@gmail.com>
2020-04-04 15:58:28 +0200

    Define environment variables for production and for development modes

--- src/api.js
+++ src/api.js
@@ -1,10 +1,6 @@
 import axios from "axios";
 import openSocket from "socket.io-client";
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
 
-console.log(process.env);
 const server = axios.create({
   baseURL:
     process.env.REACT_APP_LOCALHOST_RESTFUL ||
