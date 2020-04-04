9d3dc17340e4b400de3f3b3b76c5ff8581636005
guy490 <guy8824@gmail.com>
2020-04-04 15:35:03 +0200

    Adding .env for environment variables

--- src/api.js
+++ src/api.js
@@ -1,14 +1,18 @@
 import axios from "axios";
 import openSocket from "socket.io-client";
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-// const server = axios.create({
-//   baseURL: "http://localhost:3001"
-// });
 const server = axios.create({
-  baseURL: "https://travel-match-project.herokuapp.com/"
+  baseURL:
+    process.env.REACT_APP_LOCALHOST_RESTFUL ||
+    "https://travel-match-project.herokuapp.com/",
 });
 
-// const socket = openSocket("ws://localhost:3001");
-const socket = openSocket("wss://travel-match-project.herokuapp.com/");
+const socket = openSocket(
+  process.env.REACT_APP_LOCALHOST_WEBSOCKET ||
+    "wss://travel-match-project.herokuapp.com/"
+);
 
 export { server, socket };
